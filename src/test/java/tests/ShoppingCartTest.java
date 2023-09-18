555c228ec62c34fb7e451c98ffbe63cb093bba7e
lesiakulyk95 <lesia1995@gmail.com>
2023-09-18 17:15:00 +0300

    add new test

--- src/test/java/tests/ShoppingCartTest.java
+++ src/test/java/tests/ShoppingCartTest.java
@@ -2,13 +2,24 @@ package tests;
 
 import base.AbstractBaseTest;
 import org.testng.annotations.Test;
+import pages.BrowseProductsPage;
 import pages.HomePage;
+import pages.ProductPage;
 
 public class ShoppingCartTest extends AbstractBaseTest {
 
     @Test(description = "Check the adding and removing goods  from the cart")
     public void additionAndDeletingFromCartTest() {
         HomePage homePage = new HomePage(driver);
+        BrowseProductsPage browseProductsPages = new BrowseProductsPage(driver);
+        ProductPage productPage = new ProductPage(driver);
+
+        homePage.openAllMenShoes();
+        browseProductsPages.selectSpecificProduct(1);
+        productPage.SelectDropdownSizeButton();
+
+
+
 
     }
 }
