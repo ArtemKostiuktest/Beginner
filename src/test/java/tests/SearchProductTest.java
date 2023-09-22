c890ba481062a395f833719ba0676e151d1f280a
Sofia Shevchuk <sofiashevtcuk2.04@gmail.com>
2023-09-22 13:22:59 +0300

    add new branch

--- src/test/java/tests/SearchProductTest.java
+++ src/test/java/tests/SearchProductTest.java
@@ -18,6 +18,7 @@ public class SearchProductTest extends AbstractBaseTest {
     private final int numberOfProduct = 1;
     private final String shoesSize = "2.5";
     private final String invalidQuery = "*";
+    private final String caseInsensitiveQuery = "SK8-Hi";
     private String genderFilterText;
     private String allBillingInfo;
 
@@ -109,6 +110,16 @@ public class SearchProductTest extends AbstractBaseTest {
 
         headerFragment.fillBaseSearchField(invalidQuery);
         soft.assertThat(browseProductsPage.foundProducts());
+
+    }
+
+    @Test
+    public void searchUsingCaseInsensitiveQuery(){
+        HeaderFragment headerFragment = new HeaderFragment(driver);
+        SoftAssertions soft = new SoftAssertions();
+        BrowseProductsPage browseProductsPage = new BrowseProductsPage(driver);
+
+        headerFragment.fillBaseSearchField(caseInsensitiveQuery);
 
     }
 }
